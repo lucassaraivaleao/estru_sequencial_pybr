1add4ccc5c739f91433ead57c37abe3fe5807bd6
Lucas Saraiva Leão <lucas.saraivaleao@yahoo.com>
2022-02-07 18:32:26 -0300

    Desenvolvimento do primeiro exercicio.

---
+++ exercicio_01.py
@@ -0,0 +1,10 @@
+"""
+Exercicio-01: Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+Autor: Lucas Leão
+
+Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+"""
+
+msg = 'Alo mundo'
+print(msg)
