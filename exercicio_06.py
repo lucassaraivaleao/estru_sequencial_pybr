4a10eae1cc447adb467a0bb4c6c177e58afecc09
Lucas Saraiva Leão <lucas.saraivaleao@yahoo.com>
2022-02-07 20:14:27 -0300

    Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. Obs: A área de um círculo é pi vezes o raio elevado ao quadrado (A = π r²)

--- exercicio_06.py
+++ exercicio_06.py
@@ -2,10 +2,10 @@
 Exercicio-06:  Faça um Programa que peça o raio de um círculo, calcule e
 mostre sua área. Obs: A área de um círculo é pi vezes o raio elevado ao quadrado (A = π r²)
 Autor: Lucas Leão
+
 """
 
 import math
-
 raio = float(input('Informe o raio do círculo: '))
 area_circulo = math.pi * (raio * raio)
 print(area_circulo)
