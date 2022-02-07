51e98f120e8781574d6a58f6551441ccb84eca97
Lucas Saraiva Leão <lucas.saraivaleao@yahoo.com>
2022-02-07 20:25:29 -0300

    Faça um Programa que pergunte quanto você ganha  por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.

--- exercicio_08.py
+++ exercicio_08.py
@@ -10,5 +10,6 @@ salario = float(input('Informe seu salário: '))
 valor_horas = float(input('Informe quanto você ganha por hora trabalhada: '))
 horas_mensais = float(input('Informe a quantidade de horas trabalhadas neste mês: '))
 
-salario += horas_mensais * valor_horas
-print(f'Seu salário no final do mês será: {salario}')
+salario_final = salario + (horas_mensais * valor_horas)
+
+print(f'Seu salário no final do mês será: {salario_final}')
