9bad9c0a29975c667c1908fa94cd64f3d294e0f1
Lucas Saraiva Leão <lucas.saraivaleao@yahoo.com>
2022-02-07 20:01:16 -0300

    Faça um Programa que calcule a área de um quadrado, em seguida mostre  dobro desta área para o usuário.

---
+++ exercicio_7.py
@@ -0,0 +1,9 @@
+"""
+Exercicio-07: Faça um Programa que calcule a área de um quadrado, em seguida mostre
+ dobro desta área para o usuário.
+ Autor: Lucas Leão
+
+"""
+lado_quadrado = float(input('Informe o valor de um dos lados do quadrado: '))
+area_quadrado = lado_quadrado ** 2
+print(f'A área do quadrado é {area_quadrado} cm²')
