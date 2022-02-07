71c3b67f42e7136b0dc0cfef6f448908dba981ee
Lucas Saraiva Leão <lucas.saraivaleao@yahoo.com>
2022-02-07 19:05:17 -0300

    Faça um Programa que converta metros para centímetros.

---
+++ exercicio_5.py
@@ -0,0 +1,6 @@
+"""
+Exercicio-05: Faça um Programa que converta metros para centímetros..
+Autor: Lucas Leão
+"""
+metros = float(input('Informe os metros: '))
+print(f'{metros} m são {metros*100} cm')
