6e6e993547403a63f8033a6d59449d0a0ee9355e
Lucas Saraiva Leão <lucas.saraivaleao@yahoo.com>
2022-02-07 18:35:05 -0300

    Faça um Programa que mostre a mensagem "Alo mundo" na tela.

---
+++ exercicio_02.py
@@ -0,0 +1,6 @@
+"""
+Exercicio-01: Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+Autor: Lucas Leão
+"""
+numero = int(input('Informe um número: '))
+print(f'O número informado foi {numero}')
