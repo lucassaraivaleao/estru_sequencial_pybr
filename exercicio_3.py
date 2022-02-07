346ccec4a9885db773ac1c759ce28a4dc5d2da54
Lucas Saraiva Leão <lucas.saraivaleao@yahoo.com>
2022-02-07 18:37:12 -0300

    Faça um Programa que peça dois números e imprima a soma.

---
+++ exercicio_3.py
@@ -0,0 +1,9 @@
+"""
+Exercicio-01: Faça um Programa que peça dois números e imprima a soma.
+Autor: Lucas Leão
+"""
+
+primeiro_numero = int(input('Informe o primeiro número: '))
+segundo_numero = int(input('Informe o segundo número: '))
+soma = primeiro_numero + segundo_numero
+print(f'A soma de {primeiro_numero} + {segundo_numero} é {soma}')
